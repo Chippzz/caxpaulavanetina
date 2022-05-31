a5449a048a7bd2db43599816c4a3a4a817a6b496
Paula <104459506+Chippzz@users.noreply.github.com>
2022-05-31 14:05:59 -0500

    Add files via upload

---
+++ Python/bloque2/08-12-21.py
@@ -0,0 +1,12 @@
+
+
+while True:
+  n=int(input('adivina el numero: '))
+  if(n>26):
+    print('casi pero te pasaste')
+  if(n<26):
+      print('casi pero te falta')
+  if(n==26):
+        print('Lo lograste , felicidades')
+    
+  
