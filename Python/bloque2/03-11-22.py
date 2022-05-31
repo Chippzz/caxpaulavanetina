cda9b9915243cf649678d2c431b7c40689464aab
Paula <104459506+Chippzz@users.noreply.github.com>
2022-05-31 13:39:22 -0500

    Add files via upload

---
+++ Python/bloque2/03-11-22.py
@@ -0,0 +1,14 @@
+print("saludines")
+print('reporbados')
+variable='helo there'
+print(variable)
+print('hola es', variable )
+a=2
+b=3
+print(a+b)
+print('como te llamas?')
+nombre=input()
+print('saludos', nombre)
+
+apellido=input('cual es tu apellido? ')
+print('tu apellido es:', apellido)
