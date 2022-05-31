962eb58497b7f44a2fe3ded38d9d10d146ab9af0
Paula <104459506+Chippzz@users.noreply.github.com>
2022-05-31 14:14:54 -0500

    Add files via upload

---
+++ Python/bloque2/30-11-21 2.py
@@ -0,0 +1,14 @@
+numero=1
+
+while (numero<=4):
+  print(numero)
+  numero=numero+1
+  stop=input()
+
+numero=1
+
+while True:
+  print(infinito)
+  print(numero)
+  numero=numero+1
+  stop=input()
---
+++ Python/bloque2/30-11-21code.py
@@ -0,0 +1,10 @@
+numero=0
+
+while True:
+  print(numero)
+  numero=numero+26
+  stop=input()
+  
+
+
+  
