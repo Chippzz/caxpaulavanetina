d23c3f99c16c475814231b792dea84c96eb64add
Paula <104459506+Chippzz@users.noreply.github.com>
2022-05-31 13:59:02 -0500

    Add files via upload

---
+++ Python/bloque2/14-12-21.py
@@ -0,0 +1,125 @@
+m=1
+caja=50
+gom=30
+libro=200
+rana=60
+queso=1000
+arbol=300
+papas=20
+agua=25
+plastilina=17
+juego=20
+while (m<=1):
+  print('Hola Bienvenid@ a la tienda The moth house')
+  print("")
+  print(' A R T I C U L O S DE P R O M O C I O N:')
+  print('-----------------------------------------')
+  print('♥︎.1.Caja de Chocolates ')
+  print('♥︎.2.Gomitas de Manzana')
+  print('♥︎.3.Libro de Suspenso')
+  print('♥︎.4.Rana de Peluche')
+  print('♥︎.5.Escultura de Queso!')
+  print('♥︎.6.Árbol de Navidad')
+  print('♥︎.7.Papas')
+  print('♥︎.8.Botella de Agua')
+  print('♥︎.9.Plastilina')
+  print('♥︎.10.Juego de mesa')
+  print('--------------------------------------')
+  a=int(input('eliga el numero del articulo que desee saber el precio:'))
+  if(a==1):
+    print('el articulo que eligió cuesta: 50 pesos')
+  if(a==2):
+    print('el articulo que eligió cuesta: 30 pesos')
+  if(a==3):
+    print('el articulo que eligió cuesta: 200 pesos')
+  if(a==4):
+    print('el articulo que eligió cuesta: 60 pesos')
+  if(a==5):
+    print('el artículo que eligió cuesta: 1000')
+  if(a==6):
+    print('el artículo que eligió cuesta: 300 pesos')
+  if(a==7):
+    print('el articulo que eligió cuesta: 20 pesos')
+  if(a==8):
+    print('el artículo que eligió cuesta: 25 pesos')
+  if(a==9):
+    print('el articulo que eligió cuesta: 17 pesos')
+  if(a==10):
+    print('el articulo que eligió cuesta: 20 pesos')
+  m=m+1
+  print('')
+  p=input('desea comprar algún artículo?')
+  if(p=='no'):
+    print('ok que tenga buen día ')
+  if(p=='si'):
+    print('ok')
+    w=input('ingrese su primer articulo:')
+    if(w==1):
+      print('el precio de su primer articulo es 50 pesos')
+      cajadec=50
+    if(w==2):
+        print('el precio de su primer articulo es 30 pesos')
+        gom=30
+    if(w==3):
+      print('el precio de su primer articulo es 200 pesos')
+      libro=200
+    if(w==4):
+      print('el precio de su primer articulo es 60 pesos')
+      rana=60
+    if(w==5):
+      print('el precio de su primer articulo es 1000 pesos')
+      queso=1000
+    if(w==6):
+      print('el precio de su primer articulo es 300 pesos')
+      arbol=300
+    if(w==7):
+      print('el precio de su primer articulo es 20 pesos')
+      papas=20
+    if(w==8):
+      print('el precio de su primer articulo es 25 pesos')
+      agua=25
+    if(w==9):
+      print('el precio de su primer articulo es 17 pesos')
+      plastilina=17
+    if(w==10):
+      print('el precio de su primer articulo es 20 pesos')
+      juego=20
+      
+    
+    q=input('ingrse su segundo artículo:')
+    if(q==1):
+      print('el precio de su primer articulo es 50 pesos')
+      cajadec=50
+    if(q==2):
+        print('el precio de su primer articulo es 30 pesos')
+        gom=30
+    if(q==3):
+      print('el precio de su primer articulo es 200 pesos')
+      libro=200
+    if(q==4):
+      print('el precio de su primer articulo es 60 pesos')
+      rana=60
+    if(q==5):
+      print('el precio de su primer articulo es 1000 pesos')
+      queso=1000
+    if(w==6):
+      print('el precio de su primer articulo es 300 pesos')
+      arbol=300
+    if(w==7):
+      print('el precio de su primer articulo es 20 pesos')
+      papas=20
+    if(w==8):
+      print('el precio de su primer articulo es 25 pesos')
+      agua=25
+    if(w==9):
+      print('el precio de su primer articulo es 17 pesos')
+      plastilina=17
+    if(w==10):
+      print('el precio de su primer articulo es 20 pesos')
+      juego=20
+    
+    
+  
+
+
+
